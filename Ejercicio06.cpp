6d37599dad18de6b57b30b570edeca6d76cea880
maricielo <cielotapia198@gmail.com>
2025-07-05 01:24:07 -0500

    Ejercicio 06 : Mostrar matriz reflejada y mejorar interfaz de usuario, version final

--- Ejercicio06.cpp
+++ Ejercicio06.cpp
@@ -7,25 +7,47 @@ int main(){
 	cout << "==========================================================" << endl;
     cout << "                  Reflejar una Imagen " << endl;
     cout << "==========================================================\n" << endl;
-
+    
+    cout << "Ingrese los valores para la matriz 6x6:\n" << endl;
 	for(int i = 0; i < 6; i++){
 		for(int j = 0; j < 6; j++){
-			cout << "Ingrese los valores de la matriz en la posicion: [" << i << "] [" << j << "] :";
+			cout << "Posicion: [" << i << "] [" << j << "] : ";
 			cin >> matriz[i][j];
 		}
 	}
 	system("pause");
 	system("cls");
 	
-	cout << "Matriz actual:" << endl;
+	cout << "\n----------Matriz actual:----------\n" << endl;
 	
 	for(int i = 0; i < 6; i++){
+		cout << "         ";
 		for(int j = 0; j < 6; j++){
 			cout << matriz[i][j] << " ";
 		}
 		cout << endl;
 	}
+	cout << endl;
 	system("pause");
+	
+	cout << "\n---------Matriz reflejada:---------\n" << endl;
+	
+	int temp = 0;
+	for(int i = 0; i < 6; i++){
+	    for(int j = 0; j < 3; j++){ 
+	        temp = matriz[i][j];
+	        matriz[i][j] = matriz[i][5 - j];
+	        matriz[i][5 - j] = temp;
+	    }
+	}
+	
+	for(int i = 0; i < 6; i++) {
+		cout << "         ";
+	    for(int j = 0; j < 6; j++) {
+	        cout << matriz[i][j] << " ";
+	    }
+    	cout << endl;
+	}
 	
 	return 0;
 }
