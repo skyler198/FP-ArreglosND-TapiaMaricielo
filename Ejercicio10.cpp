d9b249296769a8e18326956a072cb54bd1fe8b92
maricielo <cielotapia198@gmail.com>
2025-07-06 18:21:25 -0500

    Ejercicio 10 – avance 1:  Crear aleatoriamente un tablero donde cada celda puede contener una mina (1) o estar vacía (0)

---
+++ Ejercicio10.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+int main() {
+    int matrizMina[10][10];
+
+    srand(time(0));
+
+    for (int i = 0; i < 10; i++){
+        for (int j = 0; j < 10; j++){
+            matrizMina[i][j] = rand() % 2;
+        }
+    }  
+    
+    for(int i = 0; i <10; i++){
+    	cout << "       ";
+    	for(int j = 0; j < 10; j++){
+    		cout << matrizMina[i][j] << " ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
