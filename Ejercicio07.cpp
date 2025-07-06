3eee87eb3d7883d8682a83f463e47fc9f2dea0ef
maricielo <cielotapia198@gmail.com>
2025-07-05 20:46:44 -0500

    Ejercicio 07 : Mostrar matriz rotada, version final

--- Ejercicio07.cpp
+++ Ejercicio07.cpp
@@ -19,12 +19,32 @@ int main() {
     system("pause");
     system("cls");
     
-    cout << "\n-------Matriz inicial:-------\n" << endl;
+    cout << "\n------- Matriz inicial: -------\n" << endl;
     
     for (int i = 0; i < 4; i++) {
         cout << "           ";
         for (int j = 0; j < 4; j++) {
             cout << matriz[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    system("pause");
+    
+    
+    int matrizRotada[4][4];
+    
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            matrizRotada[j][3 - i] = matriz[i][j];
+        }
+    }
+
+    cout << "\n----- Matriz Rotada 90 grados en sentido horario: -----\n" << endl;
+    for (int i = 0; i < 4; i++) {
+        cout << "           ";
+        for (int j = 0; j < 4; j++) {
+            cout << matrizRotada[i][j] << " ";
         }
         cout << endl;
     }
