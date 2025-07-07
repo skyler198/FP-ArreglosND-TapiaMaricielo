57cf3f98998c1f600da97bba36d75295cf3c6c4c
maricielo <cielotapia198@gmail.com>
2025-07-07 01:48:42 -0500

    Ejercicio 01 : Implementar logica para multiplicar matrices y mostrar el resultado de forma horizontal, version final

--- Ejercicio01.cpp
+++ Ejercicio01.cpp
@@ -21,5 +21,47 @@ int main() {
             cin >> B[i][j];
         }
     }
+    
+    //multiplicacion
+    for (int i = 0; i < 3; i++) {
+	    for (int j = 0; j < 3; j++) {
+	        for (int k = 0; k < 3; k++) {
+	            R[i][j] += A[i][k] * B[k][j];
+	        }
+	    }
+	}
+
+    // Mostrar resultado en horizontal
+    cout << "\nMatriz A:      Matriz B:      Resultado:\n";
+    for (int i = 0; i < 3; i++) {
+        // Matriz A
+        for (int j = 0; j < 3; j++) {
+            cout << A[i][j] << " ";
+        }
+        
+        if (i == 1) {
+        cout << "     x    ";
+	    } else {
+	        cout << "          ";
+	    }
+
+        // Matriz B
+        for (int j = 0; j < 3; j++) {
+            cout << B[i][j] << " ";
+        }
+        
+	    if (i == 1) {
+	        cout << "    =    ";
+	    } else {
+	        cout << "         ";
+	    }
+
+        // Resultado
+        for (int j = 0; j < 3; j++) {
+            cout << R[i][j] << " ";
+        }
+        cout << endl;
+    }
+
     return 0;
 }
