9345d4c4506c6bb245a9b9d0b540d02325921113
maricielo <cielotapia198@gmail.com>
2025-07-07 00:22:54 -0500

    Ejercicio 03 : Implementar calculo para la suma de diagonales y mostrar el resultado, version final

--- Ejercicio03.cpp
+++ Ejercicio03.cpp
@@ -12,5 +12,26 @@ int main (){
 	system("pause");
 	system("cls");
 
+	int DP = 0;
+	int DS = 0;
+	
+	cout << "\n----- matriz: -----\n" << endl;
+	for (int i = 0; i < 4; i++){
+		cout << "      ";
+		for (int j = 0; j < 4; j++){
+			cout << matriz[i][j] << " ";
+		}
+		cout << endl;
+	}
+	
+	cout << "\nSuma de la dos diagonales:" << endl;
+	
+	for (int i = 0; i < 4; i++){
+		DP = DP + matriz[i][i];
+		DS = DS + matriz[i][3 - i];
+	}
+	cout << endl;
+	cout << DP <<" + " << DS << " = " << DP + DS << endl;
+	
 	return 0;
 }
