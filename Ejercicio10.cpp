c700b5b0f3410ac2a01c481f4ba9aa20945c3faf
maricielo <cielotapia198@gmail.com>
2025-07-06 19:21:43 -0500

    Ejercicio 10 – avance 2: Implementar logica para calcular el numero de minas y mostrar el nuevo tablero

--- Ejercicio10.cpp
+++ Ejercicio10.cpp
@@ -21,5 +21,55 @@ int main() {
 		}
 		cout << endl;
 	}
-	return 0;
+	
+	//calcular cuantas minas hay cerca
+	/*
+	(i-1, j-1) (i-1, j) (i-1, j+1)
+	(i  , j-1)   [x]    (i  , j+1)
+	(i+1, j-1) (i+1, j) (i+1, j+1)
+	*/
+	
+	int matrizCont[10][10];
+	for(int i = 0; i < 10; i++){
+		for(int j =0; j < 10; j++){
+			int cont = 0;
+		
+			if(i - 1 >= 0 && j - 1 >= 0 && matrizMina[i - 1][j - 1] == 1){
+				cont++;	
+			}
+			if (i + 1 < 10 && j - 1 >= 0 && matrizMina[i + 1][j - 1] == 1){
+				cont++;
+			}
+			if(i - 1 >= 0 && j + 1 < 10 && matrizMina[i - 1][j + 1] == 1){
+				cont++;
+			}
+			if (i + 1 < 10 && j + 1 < 10 && matrizMina[i + 1][j + 1] == 1){
+				cont++;
+			}
+			if(i - 1 >= 0 && matrizMina[i - 1][j] == 1){
+				cont++;
+			}
+			if (i + 1 < 10 && matrizMina[i + 1][j] == 1){
+				cont++;
+			}
+			if(j - 1 >= 0 && matrizMina[i][j - 1] == 1){
+				cont++;
+			}
+			if(j + 1 < 10 && matrizMina[i][j + 1] == 1){
+				cont++;
+			}
+			
+			matrizCont[i][j] = cont;		
+		}
+	}
+	
+	cout << "\nTablero\n" << endl;
+	for(int i = 0; i < 10; i++){
+		cout << "       ";
+		for(int j = 0; j < 10; j++){
+			cout << matrizCont[i][j] << " ";
+		}
+		cout << endl;
+	}
+	return 0;	
 }
