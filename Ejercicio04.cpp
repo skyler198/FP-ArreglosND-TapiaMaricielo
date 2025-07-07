3d5bf76281dbe38af5018df2cfd5ea457869986b
maricielo <cielotapia198@gmail.com>
2025-07-07 00:51:35 -0500

    Ejercicio 04 : Implementar logica para intercambiar filas y mostrar el resultado, version final

--- Ejercicio04.cpp
+++ Ejercicio04.cpp
@@ -13,7 +13,7 @@ int main() {
     cout << "               INTERCAMBIAR FILAS" << endl,
     cout << "==============================================\n" << endl;
     
-    cout << "   ----- Matriz -----\n" << endl;
+    cout << "   ----- Matriz: -----\n" << endl;
     for (int i = 0; i < 4; i++){
 		cout << "         ";
 		for (int j = 0; j < 4; j++){
@@ -22,12 +22,43 @@ int main() {
 		cout << endl;
 	}
 
-    int fila1, fila2;
-    cout << "\nIngrese la primera fila a intercambiar (0-3): ";
-    cin >> fila1;
-    cout << "Ingrese la segunda fila a intercambiar (0-3): ";
-    cin >> fila2;
-    
+   int fila1, fila2;
 
+	do {
+	    cout << "\nIngrese la primera fila a intercambiar (0-3): ";
+	    cin >> fila1;
+	
+	    if (fila1 < 0 || fila1 > 3) {
+	        cout << "Respuesta invalida, ingrese un numero entre 0 y 3." << endl;
+	    }
+	
+	} while (fila1 < 0 || fila1 > 3);
+	
+	do {
+	    cout << "Ingrese la segunda fila a intercambiar (0-3): ";
+	    cin >> fila2;
+	
+	    if (fila2 < 0 || fila2 > 3) {
+	        cout << "Respuesta invalida, ingrese un numero entre 0 y 3." << endl;
+	    }
+	
+	} while (fila2 < 0 || fila2 > 3);
+
+    
+	int temp = 0;
+    for (int j = 0; j < 4; j++) {
+        temp = matriz[fila1][j];
+        matriz[fila1][j] = matriz[fila2][j];
+        matriz[fila2][j] = temp;
+    }
+
+    cout << "\n    --- Matriz final: ---\n" << endl;
+    for (int i = 0; i < 4; i++){
+    	cout << "          ";
+        for (int j = 0; j < 4; j++){
+            cout << matriz[i][j] << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }
